f5daf3a888db603305f8d81947229ddc636e45b4
Kenny Cordero <GeckoB4I@gmail.com>
2023-01-03 19:22:40 -0600

    finally, after arduous battle with myself and my PC, I implemented an idea to keep data after refresh

--- assets/script.js
+++ assets/script.js
@@ -1,42 +1,32 @@
 
 $(function () {
-
-  // TODO: Add a listener for click events on the save button. 
+  //Add a listener for click events on the save button. 
   $('.saveBtn').on('click', (event) => {
-    //This code should use the id in the containing time-block as a key to save the user input in local storage.
-    console.log("save-button click event check")
     event.preventDefault();
-    let key = $(event.currentTarget).closest('div').attr('id') // --> This will be the key
 
-    let value = $('.description').val(); // --> the value (this is what the user writes in the textarea) 
+    //SETTING THE KEY AND VALUE
+    //Setting the Key
+    let key = $(event.currentTarget).parent().attr('id');
+    console.log(`The key is ${key}.`)
+    //This logs the 'textArea' id: '9Time' -> string
 
-    console.log(key);
-    // console.log(typeof(key));
-    console.log(value);
+    //Setting the Value
+    let value = $(event.currentTarget).prev().val();
+    console.log(`This value is ${value}.`)
+    //This logs the data the user passes into the textArea: 'asd' -> string
 
+    //Setting the local storage
     localStorage.setItem(key, value);
-
-    //trying to populate the textarea with the notes saved in local storage when browser refreshes
-    $noteValue = $('.description').val();
-    $noteValueLocalStorage = localStorage.getItem(key);
-
-    console.log(`The local storage is: ${JSON.stringify(localStorage)}`);
-    console.log(`The $noteValue is: ${$noteValue}`);
-    console.log(`the $noteValueLocalStorage is: ${$noteValueLocalStorage}`)
-    
+    console.log(`Local Storage after assigning the key and value: ${JSON.stringify(localStorage)}`)
   })
 
-  
-  // HINT: What does `this` reference in the click listener function? 
-  
-  // How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked? 
-  
-  // How might the id be useful when saving the description in local storage?
-  
-  // TODO: Add code to apply the past, present, or future class to each time block by comparing the id to the current hour. 
-
+  //Checks to see if any values are saved in local storage, OUTSIDE the event listener...
+  for (let i = 9; i <= 17; i++) {
+    $(`#${i}`).val(localStorage.getItem(`${i}Time`));
+  }
+ 
+  //Loops through the time-blocks, checks the current time, and updates the blocks accordingly
   currentHour = dayjs().$H;
-
   for (let i = 9; i <= 17; i++) {
     if (i === currentHour) {
       $(`#${i}`).addClass('present')
@@ -46,19 +36,7 @@ $(function () {
       $(`#${i}`).addClass('past')
     }
   }
- 
-
-  //How can Day.js be used to get the current hour in 24-hour time?
-
-  // TODO: Add code to get any user input that was saved in localStorage and set the values of the corresponding textarea elements. 
-  
-  // HINT: How can the id attribute of each time-block be used to do this?
-
-  // TODO: Add code to display the current date in the header of the page.
-  // console.log(dayjs()); //This works
-
-  //Location to put todays date
+  //Displays the current time
   const $dateLocation = $('#currentDay')
-  
   $dateLocation.text(dayjs().$d)
 });
